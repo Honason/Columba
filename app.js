e16c0ed2534c63b863956e5868e41ba2c7a4df72
Honason <honason@gmail.com>
2015-04-26 21:08:54 +0200

    fixes

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.PWD = process.cwd();
 mongoose.connect('mongodb://root:root@ds039311.mongolab.com:39311/node-workshop');
 
 var routes = require('./routes/index');
